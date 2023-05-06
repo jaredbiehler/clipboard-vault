6f75c81fb3dcfc36d79e0b45a2bc132e0eaef7ef
Jared Biehler <jaredbcoding@gmail.com>
2023-05-05 21:53:18 -0400

    Initial Commit

---
+++ ClipboardVault/ClipboardVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardVaultApp.swift
+//  ClipboardVault
+//
+//  Created by Jared Biehler on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
